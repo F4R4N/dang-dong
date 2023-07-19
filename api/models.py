619952034d1d6a88428374610272a3ade52e9b19
Faran <farantgh@gmail.com>
2023-07-19 23:31:49 +0330

    add translation and verbose name

--- api/models.py
+++ api/models.py
@@ -2,37 +2,44 @@ from django.db import models
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from django.core.validators import MinValueValidator
+from django.utils.translation import gettext_lazy as _
 
 
 class Period(models.Model):
-    name = models.CharField(max_length=100)
-    start_date = models.DateField(default=timezone.now)
-    owner = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+    slug = models.CharField(verbose_name=_("Slug"), unique=True, primary_key=True, max_length=256, editable=False)
+    name = models.CharField(verbose_name=_("Name"), max_length=100)
+    start_date = models.DateTimeField(verbose_name=_("Start Date"), default=timezone.now)
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("Owner"), editable=False)
+    # TODO: trigger slug change on user username change
+
+    def save(self, *args, **kwargs):
+        self.slug = self.owner.username + "-" + self.name
+        return super().save()
 
     def __str__(self):
         return self.name
 # TODO: check period name uniqueness in serializers based on owners period
-# TODO: add a method to calculate all expences
+# TODO: add a method to calculate all expenses
 
 
 class Person(models.Model):
-    name = models.CharField(max_length=100)
-    user = models.ForeignKey(get_user_model(), blank=True, on_delete=models.DO_NOTHING, null=True)
-    periods = models.ForeignKey(Period, on_delete=models.DO_NOTHING)
-    coefficient = models.IntegerField(default=1, validators=[MinValueValidator(1)])
+    name = models.CharField(verbose_name=_("Name"), max_length=100)
+    user = models.ForeignKey(get_user_model(), blank=True, on_delete=models.DO_NOTHING, null=True, verbose_name=_("User"))
+    period = models.ForeignKey(Period, on_delete=models.DO_NOTHING, verbose_name=_("Period"))
+    coefficient = models.IntegerField(verbose_name=_("Coefficient"), default=1, validators=[MinValueValidator(1)])
 
     def __str__(self):
         return self.name
-# TODO: check user name uniqueness in serializers based on users in period
+# TODO: check user name uniqueness in serializers based on users in "period"
 
 
 class Purchase(models.Model):
-    name = models.CharField(max_length=100)
-    date_and_time = models.DateTimeField(default=timezone.now)
-    expense = models.PositiveBigIntegerField()
-    buyer = models.ForeignKey(Person, on_delete=models.PROTECT)
-    purchase_users = models.ManyToManyField(Person)
-    period = models.ForeignKey(Period, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
+    date_and_time = models.DateTimeField(default=timezone.now, verbose_name=_("Date and Time"))
+    expense = models.PositiveBigIntegerField(verbose_name=_("Expense"))
+    buyer = models.ForeignKey(Person, on_delete=models.PROTECT, verbose_name=_("Buyer"), related_name="purchase_set_buyer")
+    purchase_users = models.ManyToManyField(Person, verbose_name=_("Users"))
+    period = models.ForeignKey(Period, on_delete=models.CASCADE, verbose_name=_("Period"))
 
     def __str__(self):
         return self.name
