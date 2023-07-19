f6eb5116546daab648c8beaec3c35123fc201bc4
Faran <farantgh@gmail.com>
2023-07-19 23:30:54 +0330

    add purchase

--- api/admin.py
+++ api/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
-from .models import Period, Person
+from .models import Period, Person, Purchase
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-	list_display = ("name", "user", "periods", "coefficient")
+	list_display = ("name", "user", "period", "coefficient")
 
 
 @admin.register(Period)
 class PeriodAdmin(admin.ModelAdmin):
 	list_display = ("name", "start_date", "owner")
+
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+	list_display = ("name", "date_and_time", "expense", "buyer", "get_purchase_users", "period")
+	
+	def get_purchase_users(self, instance):
+		result = []
+		for person in instance.purchase_users.all():
+			result.append(person.name)
+		return result
