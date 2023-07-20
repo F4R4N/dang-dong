389cd69efc4b6e3eee52454846ba0869e42f6d10
Faran <farantgh@gmail.com>
2023-07-20 22:53:09 +0330

    add update, destroy, list to the period view

--- api/views.py
+++ api/views.py
@@ -1,13 +1,29 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from .serializers import PeriodSerializer
 from .models import Period
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from .permissions import IsOwner
+from .responses import RESPONSE_MESSAGES
 
 
 class PeriodViewSet(viewsets.ModelViewSet):
     queryset = Period.objects.all()
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, IsOwner)
     serializer_class = PeriodSerializer
 
     def perform_create(self, serializer):
         serializer.save()
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+    def destroy(self, request, pk=None):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT, data={"detail": RESPONSE_MESSAGES["successfully_deleted"]})
+
+    def list(self, request):
+        periods = Period.objects.filter(owner=request.user)
+        serializer = self.get_serializer(periods, many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
