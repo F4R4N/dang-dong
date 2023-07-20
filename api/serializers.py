9ef01f203dfeb3210a0b918233e589ad7659e7f5
Faran <farantgh@gmail.com>
2023-07-20 22:52:42 +0330

    add update and validate name

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Period
-from .errors import ERROR_MESSAGES
+from .responses import ERROR_MESSAGES
 
 
 class PeriodSerializer(serializers.ModelSerializer):
@@ -9,12 +9,25 @@ class PeriodSerializer(serializers.ModelSerializer):
         fields = ("slug", "name", "start_date", "owner")
 
     def validate_name(self, value):
-        user = self.context.get("request").user
-        if Period.objects.filter(name=value, owner=user).exists():
-            raise serializers.ValidationError(ERROR_MESSAGES["unique_period_name_per_user"])
+        request = self.context.get("request")
+        if request.method == "POST":
+            if Period.objects.filter(name=value, owner=request.user).exists():
+                raise serializers.ValidationError(ERROR_MESSAGES["unique_period_name_per_user"])
+        if request.method == "PUT":
+            instance = self._args[0]
+            if instance.name != value:
+                if Period.objects.filter(name=value, owner=request.user).exists():
+                    raise serializers.ValidationError(ERROR_MESSAGES["unique_period_name_per_user"])
         return value
 
     def create(self, validated_data):
         owner = self.context.get("request").user
         return Period.objects.create(**validated_data, owner=owner)
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get("name", instance.name)
+        instance.star_date = validated_data.get("start_date", instance.start_date)
+        instance.save()
+        return instance
+
 
