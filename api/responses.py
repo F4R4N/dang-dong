e7ce76501c1f792b53d347145ea1d0c390fa17fa
Faran <farantgh@gmail.com>
2023-07-21 23:43:32 +0330

    rename

--- api/responses.py
+++ api/responses.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 ERROR_MESSAGES = {
-    "unique_period_name_per_user": _("This field should be unique")
+    "unique_field": _("This field should be unique"),
 }
 
 RESPONSE_MESSAGES = {
