da993b5c85f65e0c300199aff52f76059a98c74c
Faran <farantgh@gmail.com>
2023-07-29 22:13:36 +0330

    fix period models endpoints

--- api/serializers.py
+++ api/serializers.py
@@ -1,13 +1,28 @@
 from rest_framework import serializers, exceptions
 from .models import Period, Person, Coefficient, Purchase
 from .responses import ERROR_MESSAGES
-from django.core.validators import MinValueValidator
+
+
+class PersonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Person
+        fields = ("slug", "name", "user", "owner")
+
+    def validate_name(self, value):
+        request = self.context["request"]
+        if Person.objects.filter(name=value, owner=request.user).exists():
+            raise serializers.ValidationError(ERROR_MESSAGES["unique_field"])
+        return value
+
+    def create(self, validated_data):
+        owner = self.context["request"].user
+        return Person.objects.create(**validated_data, owner=owner)
 
 
 class PeriodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Period
-        fields = ("slug", "name", "start_date", "owner")
+        fields = ("slug", "name", "start_date", "owner", "persons")  # TODO : follow up here
 
     def validate_name(self, value):
         request = self.context.get("request")
@@ -20,16 +35,31 @@ class PeriodSerializer(serializers.ModelSerializer):
                 if Period.objects.filter(name=value, owner=request.user).exists():
                     raise serializers.ValidationError(ERROR_MESSAGES["unique_field"])
         return value
+    
+    def validate_persons(self, value):
+        user = self.context["request"].user
+        for person in value:
+            if person.owner != user:
+                raise exceptions.PermissionDenied()
+
+        return value
 
     def create(self, validated_data):
         owner = self.context.get("request").user
-        return Period.objects.create(**validated_data, owner=owner)
+        persons = validated_data.pop("persons")
+        period = Period.objects.create(**validated_data, owner=owner)
+        period.persons.set(persons)
+        return period
 
     def update(self, instance, validated_data):
         instance.name = validated_data.get("name", instance.name)
         instance.star_date = validated_data.get("start_date", instance.start_date)
+        instance.persons.set(validated_data.get("persons", instance.persons.all()))
         instance.save()
         return instance
+    
+    def get_related_persons(self, obj):
+        print(obj)
 
 
 class PurchaseSerializer(serializers.ModelSerializer):
@@ -56,39 +86,17 @@ class PurchaseSerializer(serializers.ModelSerializer):
                 raise exceptions.PermissionDenied()
         return value
 
+    def validate(self, attrs):
+        persons = attrs["period"].person_set.all()  # all persons tha are related to this period
+        if attrs.get("buyer") not in persons:
+            raise serializers.ValidationError(ERROR_MESSAGES["not_period_member"])
+        for user in attrs.get("purchased_for_users"):
+            if user not in persons:
+                raise serializers.ValidationError(ERROR_MESSAGES["not_period_member"])
+        return attrs
+
     def create(self, validated_data):
         purchased_for_users = validated_data.pop("purchased_for_users")
         purchase = Purchase.objects.create(**validated_data)
         purchase.purchased_for_users.set(purchased_for_users)
         return purchase
-    
-
-
-class PersonSerializer(serializers.ModelSerializer):
-    coefficient = serializers.IntegerField(validators=[MinValueValidator(1)], required=True)
-
-    class Meta:
-        model = Person
-        fields = ("slug", "name", "user", "period", "coefficient")
-
-    def validate_period(self, value):
-        request = self.context["request"]
-        if request.method in ["POST", "PUT"]:
-            if value.owner != request.user:
-                raise exceptions.PermissionDenied()
-        return value
-
-    def validate(self, attrs):
-        request = self.context["request"]
-        if Person.objects.filter(name=attrs.get("name"), user=request.user).exists():
-            raise serializers.ValidationError({"name": ERROR_MESSAGES["unique_field"]})
-        return attrs
-
-    def create(self, validated_data):
-        return Person.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get("name", instance.name)
-        instance.coefficient = validated_data.get("name", instance.coefficient)
-        instance.save()
-        return instance
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, status
+from rest_framework.views import APIView
 from .serializers import PeriodSerializer, PersonSerializer, PurchaseSerializer
 from .models import Period, Person, Purchase
 from rest_framework.response import Response
@@ -31,17 +32,25 @@ class PeriodViewSet(viewsets.ModelViewSet):
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = Person.objects.all()
-    permission_classes = (IsAuthenticated, )
+    permission_classes = (IsAuthenticated, IsOwner)
     serializer_class = PersonSerializer
 
     def perform_create(self, serializer):
         serializer.save()
 
-    def perform_update(self, serializer):
-        serializer.save()
+    # FIXME: REMEMBER TO IMPLIMENT THIS, THE ONLY THING THA CAN BE EDITED SHOULD BE TEH COEFFICIENT OF COEFFICIENT OBJECT
+    # def perform_update(self, serializer):
+    #     serializer.save()
+
+    def destroy(self, request, pk=None):
+        instance = self.get_object()
+        self.perform_destroy(instance=instance)
+        return Response(status=status.HTTP_204_NO_CONTENT, data={RESPONSE_MESSAGES["successfully_deleted"]})
 
     def list(self, request):
-        persons = Person.objects.filter(period__owner=request.user)
+        persons = Person.objects.filter(owner=request.user)
+        serializer = self.get_serializer(persons, many=True)
+        return Response(status=status.HTTP_200_OK, data=serializer.data)
 
 
 class PurchaseViewSet(viewsets.ModelViewSet):
