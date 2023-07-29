caf89dbfd66e338b189e4b6436c08e891ff90866
Faran <farantgh@gmail.com>
2023-07-29 22:11:34 +0330

    add persons field to periods and add owner to person

--- api/models.py
+++ api/models.py
@@ -6,26 +6,27 @@ from django.utils.translation import gettext_lazy as _
 from .utils import generate_slug
 
 
+class Person(models.Model):
+    slug = models.SlugField(verbose_name=_("Slug"), unique=True, primary_key=True, editable=False, default=generate_slug)
+    name = models.CharField(verbose_name=_("Name"), max_length=100, blank=False, null=False)
+    user = models.ForeignKey(get_user_model(), blank=True, on_delete=models.DO_NOTHING, null=True, verbose_name=_("User"))
+    owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("Owner"), editable=False, related_name="owner_set")
+
+    def __str__(self):
+        return self.name
+# TODO: check user name uniqueness in serializers based on users in "period"
+
+
 class Period(models.Model):
     slug = models.SlugField(verbose_name=_("Slug"), unique=True, primary_key=True, max_length=256, editable=False, default=generate_slug)
     name = models.CharField(verbose_name=_("Name"), max_length=100)
     start_date = models.DateTimeField(verbose_name=_("Start Date"), default=timezone.now)
     owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("Owner"), editable=False)
+    persons = models.ManyToManyField(Person, verbose_name=_("Person"), blank=True)
 
     def __str__(self):
         return self.name
 # TODO: add a method to calculate all expenses
-
-
-class Person(models.Model):
-    slug = models.SlugField(verbose_name=_("Slug"), unique=True, primary_key=True, editable=False, default=generate_slug)
-    name = models.CharField(verbose_name=_("Name"), max_length=100, blank=False, null=False)
-    user = models.ForeignKey(get_user_model(), blank=True, on_delete=models.DO_NOTHING, null=True, verbose_name=_("User"))
-    period = models.ForeignKey(Period, on_delete=models.DO_NOTHING, verbose_name=_("Period"), null=False, blank=False)
-
-    def __str__(self):
-        return self.name
-# TODO: check user name uniqueness in serializers based on users in "period"
 
 
 class Purchase(models.Model):
