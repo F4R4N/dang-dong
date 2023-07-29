755e275f828001679163b2dd04893ff6ae4f199f
Faran <farantgh@gmail.com>
2023-07-29 22:11:46 +0330

    add not_period_member

--- api/responses.py
+++ api/responses.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 ERROR_MESSAGES = {
     "unique_field": _("This field should be unique"),
+    "not_period_member": _("Persons should be a member of this period first.")
 }
 
 RESPONSE_MESSAGES = {
