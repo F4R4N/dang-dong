8282ba85493eccf428afdb05c97d33994dc318f2
Faran <farantgh@gmail.com>
2023-07-31 04:06:44 +0330

    add new error messages

--- api/responses.py
+++ api/responses.py
@@ -2,8 +2,11 @@ from django.utils.translation import gettext_lazy as _
 
 
 ERROR_MESSAGES = {
-    "unique_field": _("This field should be unique"),
-    "not_period_member": _("Persons should be a member of this period first.")
+    "unique_field": _("This field should be unique."),
+    "not_period_member": _("Persons should be a member of this period first."),
+    "required_field": _("This field is required."),
+    "permission_denied": _('You do not have permission to perform this action.')
+
 }
 
 RESPONSE_MESSAGES = {
