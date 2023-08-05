aa5d4fdbcea9213b3739cb460adb3002ce620908
Faran <farantgh@gmail.com>
2023-08-05 17:18:23 +0330

    add docstrig and cleanup

--- api/permissions.py
+++ api/permissions.py
@@ -2,10 +2,15 @@ from rest_framework import permissions
 
 
 class IsOwner(permissions.BasePermission):
+    """ checks if owner of the object is the authenticated user or not."""
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
 
 
 class IsPurchaseOwner(permissions.BasePermission):
+    """ purchase object does not have owner field. this permission checks if
+    purchases related periods owner is authenticated user or not.
+    """
     def has_object_permission(self, request, view, obj):
+        print(obj.period.owner == request.user)
         return obj.period.owner == request.user
--- api/urls.py
+++ api/urls.py
@@ -9,5 +9,5 @@ router.register("person", PersonViewSet)
 router.register("purchase", PurchaseViewSet)
 
 urlpatterns = [
-    path("", include(router.urls)),    
+    path("", include(router.urls)),
 ]
