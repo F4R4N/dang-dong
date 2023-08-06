9f181c7687635d097bd027638d483e27f8439b97
Faran <farantgh@gmail.com>
2023-08-06 05:27:30 +0330

    add endpoint urls

--- customauth/urls.py
+++ customauth/urls.py
@@ -1,8 +1,16 @@
-from django.urls import path
-from .views import UserLoginView
-
+from django.urls import path, include
+from .views import LogoutView, MagicLinkView, UserViewSet
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework import routers
 app_name = "customauth"
 
+router = routers.SimpleRouter()
+router.register("user", UserViewSet)
+
 urlpatterns = [
-    path("login/", UserLoginView.as_view()),
+    path('login/magic/', MagicLinkView.as_view(), name='auth_magic_login'),
+    path('login/magic/<code>/', MagicLinkView.as_view(), name="auth_magic_code"),
+    path('login/refresh/', TokenRefreshView.as_view(), name='auth_refresh_token'),
+    path('logout/', LogoutView.as_view(), name='auth_logout'),
+    path("", include(router.urls)),
 ]
