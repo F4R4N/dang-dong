c3ac4e929c79379c6e7b5ccd9bfbcad3acf92110
Faran <farantgh@gmail.com>
2023-08-06 05:26:16 +0330

    add custom user and verification

--- customauth/models.py
+++ customauth/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import secrets
+from config.settings import AUTH_CODE_EXPIRES_IN
+from django.utils import timezone
+from api.utils import generate_id
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class User(AbstractUser):
+    id = models.SlugField(max_length=100, default=generate_id, unique=True, primary_key=True)
+    email = models.EmailField(blank=False, null=False, unique=True)
+    username = models.CharField(unique=True, max_length=250, validators=[UnicodeUsernameValidator()])
+
+
+class Verification(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    code = models.CharField(max_length=100, unique=True)
+    created_time = models.DateTimeField()
+    expire_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        self.created_time = timezone.now()
+        self.expire_at = self.created_time + AUTH_CODE_EXPIRES_IN
+        self.code = secrets.token_urlsafe(20)
+        return super().save()
+
+    def is_expired(self):
+        if self.expire_at > timezone.now():
+            return False
+        return True
