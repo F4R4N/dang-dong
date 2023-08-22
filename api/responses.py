e571fa0c49befd7ce9f3648f6d5207b0b44b45c9
Faran <farantgh@gmail.com>
2023-08-22 21:35:20 +0330

    add new response messages

--- api/responses.py
+++ api/responses.py
@@ -5,7 +5,9 @@ ERROR_MESSAGES = {
     "unique_field": _("This field should be unique."),
     "not_period_member": _("Persons should be a member of this period first."),
     "required_field": _("This field is required."),
-    "permission_denied": _('You do not have permission to perform this action.')
+    "permission_denied": _('You do not have permission to perform this action.'),
+    "person_object_protected": _("you cant remove these persons, some of the persons are part of purchases. try deleting person from that purchases and try again."),
+    "already_exists": _("user with this email or username is already exists, try another one.")
 
 }
 
