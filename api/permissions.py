cfc237128c9f633e474630b55dd163d6cf8f66b7
Faran <farantgh@gmail.com>
2023-08-22 21:35:07 +0330

    add IsAuthorizedUser

--- api/permissions.py
+++ api/permissions.py
@@ -1,8 +1,11 @@
+from typing import Any
 from rest_framework import permissions
+from rest_framework.request import Request
+from rest_framework.views import APIView
 
 
 class IsOwner(permissions.BasePermission):
-    """ checks if owner of the object is the authenticated user or not."""
+    """ checks if owner of the object is the authenticated user or not. """
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
 
@@ -14,3 +17,9 @@ class IsPurchaseOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         print(obj.period.owner == request.user)
         return obj.period.owner == request.user
+
+
+class IsAuthorizedUser(permissions.BasePermission):
+    """ make sure users can only edit their own user objects. """
+    def has_object_permission(self, request: Request, view: APIView, obj: Any) -> bool:
+        return obj == request.user
