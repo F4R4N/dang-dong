11bbac645cfba927cf4d97823433b45f24025230
Faran <farantgh@gmail.com>
2023-09-04 11:51:09 +0330

    add docstring

--- api/models.py
+++ api/models.py
@@ -7,7 +7,16 @@ from .utils import generate_id
 
 
 class Person(models.Model):
-    id = models.SlugField(verbose_name=_("id"), unique=True, primary_key=True, editable=False, default=generate_id)
+    """
+    This model represents a Person. It contains fields for the person's id, name, user, and owner (user that own that is authorized
+    to add that person to its periods and purchases).
+    Attributes:
+        id (SlugField): Unique identifier for the person.
+        name (CharField): Name of the person.
+        user (ForeignKey): :model:`customauth.User` associated with the person.
+        owner (ForeignKey): :model:`customauth.User` that owns of the person.
+    """
+    id = models.SlugField(verbose_name=_("Id"), unique=True, primary_key=True, editable=False, default=generate_id)
     name = models.CharField(verbose_name=_("Name"), max_length=100, blank=False, null=False)
     user = models.ForeignKey(get_user_model(), blank=True, on_delete=models.DO_NOTHING, null=True, verbose_name=_("User"))
     owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("Owner"), editable=False, related_name="owner_set")
@@ -17,7 +26,16 @@ class Person(models.Model):
 
 
 class Period(models.Model):
-    id = models.SlugField(verbose_name=_("id"), unique=True, primary_key=True, max_length=256, editable=False, default=generate_id)
+    """
+    This model represents a period of time which the purchased happens in, with a name, start date, owner and persons associated with it.
+    Attributes:
+        id (SlugField): A unique identifier for the period.
+        name (CharField): The name of the period.
+        start_date (DateTimeField): The start date of the period.
+        owner (ForeignKey): The :model:`customauth.User` who owns the period.
+        persons (ManyToManyField): :model:`api.Person` associated with the period.
+    """
+    id = models.SlugField(verbose_name=_("Id"), unique=True, primary_key=True, max_length=256, editable=False, default=generate_id)
     name = models.CharField(verbose_name=_("Name"), max_length=100)
     start_date = models.DateTimeField(verbose_name=_("Start Date"), default=timezone.now)
     owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, verbose_name=_("Owner"), editable=False)
@@ -28,6 +46,16 @@ class Period(models.Model):
 
 
 class Purchase(models.Model):
+    """
+    This model represents a Purchase. It contains information about the purchase such as its name, date and time, expense, buyer, and period.
+    Attributes:
+        id (SlugField): Unique identifier for the purchase.
+        name (CharField): Name of the purchase.
+        date_and_time (DateTimeField): Date and time when the purchase was made.
+        expense (PositiveBigIntegerField): Amount spent on the purchase.
+        buyer (ForeignKey): :model:`api.Person` who made the purchase.
+        period (ForeignKey): :model:`api.Period` in which the purchase was made.
+    """
     id = models.SlugField(verbose_name=_("id"), unique=True, primary_key=True, editable=False, default=generate_id)
     name = models.CharField(max_length=100, verbose_name=_("Name"))
     date_and_time = models.DateTimeField(default=timezone.now, verbose_name=_("Date and Time"))
@@ -40,10 +68,18 @@ class Purchase(models.Model):
 
 
 class PurchaseMembership(models.Model):
+    """
+    This model represents a PurchaseMembership, which is used to store information about a person's each purchase in each period.
+    Attributes:
+        id (SlugField): A unique identifier for the membership.
+        coefficient (IntegerField): The coefficient of the person membership in this purchase.
+        person (ForeignKey): The :model:`api.Person` associated with the membership.
+        purchase (ForeignKey): The :model:`api.Purchase` associated with the membership.
+    """
     id = models.SlugField(verbose_name=_("id"), unique=True, primary_key=True, editable=False, default=generate_id)
     coefficient = models.IntegerField(verbose_name=_("Coefficient"), default=1, validators=[MinValueValidator(1)])
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
-    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE, related_name="purchased_for_users")
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, verbose_name=_("Person"),)
+    purchase = models.ForeignKey(Purchase, on_delete=models.CASCADE, related_name="purchased_for_users", verbose_name=_("Purchase"))
 
     def __str__(self) -> str:
         return str(self.coefficient)
--- api/utils.py
+++ api/utils.py
@@ -13,6 +13,18 @@ def generate_id() -> str:
 
 
 def purchase_detail_data_generator(person: object, owe_to: list, direct_cost: int, final_cost: int, creditor_of: list) -> dict:
+    """
+    generates purchase detail data in a certain format.
+    Args:
+        person (object): The person making the purchase.
+        owe_to (list): A list of people the purchaser owes money to.
+        direct_cost (int): The cost of the purchase before any additional fees.
+        final_cost (int): The total cost of the purchase after all fees and taxes.
+        creditor_of (list): A list of people the purchaser is a creditor of.
+
+    Returns:
+        dict: A dictionary containing details of the purchase.
+    """
     return {
         "person": person,
         "owe_to": owe_to,
@@ -23,6 +35,21 @@ def purchase_detail_data_generator(person: object, owe_to: list, direct_cost: in
 
 
 def purchase_detail_calculator(purchase: 'Purchase') -> list:
+    """
+    Calculates the purchase details for a given purchase.
+
+    Args:
+        purchase (Purchase): The purchase to calculate the details for.
+
+    Returns:
+        list: A list of purchase detail data objects.
+            format:
+                "person": person,
+                "owe_to": owe_to,
+                "direct_cost": direct_cost,
+                "final_cost": final_cost,
+                "creditor_of": creditor_of
+    """
     PurchaseMembership = apps.get_model("api", "PurchaseMembership")
     data = []
     purchased_for_users_purchase_membership = PurchaseMembership.objects.filter(purchase=purchase)
@@ -57,6 +84,17 @@ def purchase_detail_calculator(purchase: 'Purchase') -> list:
 
 
 def get_dict_index(lst: list, key: str, value: object) -> int:
+    """
+    Returns the index of the dictionary in the given list which contains the given key-value pair.
+
+    Args:
+        lst (list): The list of dictionaries to search through.
+        key (str): The key to search for in the dictionaries.
+        value (object): The value associated with the given key.
+
+    Returns:
+        int: The index of the dictionary containing the given key-value pair, or -1 if not found.
+    """
     for index, dic in enumerate(lst):
         if dic[key] == value:
             return index
@@ -64,6 +102,16 @@ def get_dict_index(lst: list, key: str, value: object) -> int:
 
 
 def owe_and_credit_calculator(owe_or_credits: list[dict[str, object | int]]) -> list[dict[str, object | int]]:
+    """Calculates the total amount owed or credited by each person in a given list of transactions.
+
+    Args:
+        owe_or_credits (list[dict[str, object | int]]): A list of dictionaries containing information about each transaction.
+        Each dictionary should contain two keys: "person" and "amount".
+
+    Returns:
+        list[dict[str, object | int]]: A list of dictionaries containing the total amount owed or credited by each person.
+        Each dictionary contains two keys: "person" and "amount".
+    """
     data: list[dict[str, object | int]] = []
     for owe_or_credit in owe_or_credits:
         if any(element["person"] == owe_or_credit["person"] for element in data):
